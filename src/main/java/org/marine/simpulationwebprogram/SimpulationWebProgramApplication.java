9d7ab42a02195aa4b3b6b18393712b794d3645ee
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-10-23 15:44:10 +0630

    해양 드론

---
+++ src/main/java/org/marine/simpulationwebprogram/SimpulationWebProgramApplication.java
@@ -0,0 +1,13 @@
+package org.marine.simpulationwebprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpulationWebProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpulationWebProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/marine/simpulationwebprogram/SimpulationWebProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marine.simpulationwebprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpulationWebProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
