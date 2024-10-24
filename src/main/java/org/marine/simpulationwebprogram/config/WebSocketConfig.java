82a63db8998586b4ffafec618fb5722289c48d9f
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-10-24 15:59:13 +0630

    코드 및 기타 필요한 코드를 변경합니다. 코드 이상을 추가하고,일부 코드에 대해 약속을 하고,부 코드를 삭제합니다.

--- src/main/java/org/marine/simpulationwebprogram/config/WebSocketConfig.java
+++ src/main/java/org/marine/simpulationwebprogram/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package org.marine.simpulationwebprogram.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
@@ -16,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/process-csv").withSockJS(); // Enable SockJS fallback
+        registry.addEndpoint("/process-csv").withSockJS();
+        registry.addEndpoint("/server-dashboard").withSockJS(); // WebSocket endpoint
     }
 }
