1a6e85cde161cad7b3e3fee7049aae36032b59b0
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-10-24 15:58:28 +0630

    코드 및 기타 필요한 코드를 변경합니다. 코드 이상을 추가하고,일부 코드에 대해 약속을 하고,부 코드를 삭제합니다.

--- src/main/java/org/marine/simpulationwebprogram/handler/CSVWebSocketHandler.java
+++ src/main/java/org/marine/simpulationwebprogram/handler/CSVWebSocketHandler.java
@@ -1,18 +1,26 @@
 package org.marine.simpulationwebprogram.handler;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Component
 public class CSVWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        // Broadcast the received CSV message to the WebSocket client
-        String payload = message.getPayload();
-        System.out.println("Received WebSocket message: " + payload);
+        String receivedData = message.getPayload();
 
-        // Send the received message back to the client
-        session.sendMessage(new TextMessage(payload));
+        // Log received CSV row on server
+        System.out.println("Received CSV row: " + receivedData);
+
+        // If the message is "End of CSV", notify the server
+        if ("End of CSV".equals(receivedData)) {
+            session.sendMessage(new TextMessage("All rows received successfully"));
+        } else {
+            // Broadcast the received row back to the client dashboard
+            session.sendMessage(new TextMessage("Processed: " + receivedData));
+        }
     }
 }
