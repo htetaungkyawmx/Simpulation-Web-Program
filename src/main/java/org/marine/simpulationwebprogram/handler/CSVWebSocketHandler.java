c4349758744ce168cddb52502825d45921e9191a
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-10-25 14:10:15 +0630

    삭제된 스프링 보안 코드

--- src/main/java/org/marine/simpulationwebprogram/handler/CSVWebSocketHandler.java
+++ src/main/java/org/marine/simpulationwebprogram/handler/CSVWebSocketHandler.java
@@ -46,30 +46,3 @@ public class CSVWebSocketHandler extends TextWebSocketHandler {
         }
     }
 }
-
-/*
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-@Component
-public class CSVWebSocketHandler extends TextWebSocketHandler {
-
-    @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String receivedData = message.getPayload();
-
-        // Log received CSV row on server
-        System.out.println("Received CSV row: " + receivedData);
-
-        // If the message is "End of CSV", notify the server
-        if ("End of CSV".equals(receivedData)) {
-            session.sendMessage(new TextMessage("All rows received successfully"));
-        } else {
-            // Broadcast the received row back to the client dashboard
-            session.sendMessage(new TextMessage("Processed: " + receivedData));
-        }
-    }
-}
-*/
