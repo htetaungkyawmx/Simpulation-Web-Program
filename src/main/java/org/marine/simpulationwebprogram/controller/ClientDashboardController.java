3d579b6d537bca44fa848e1679817a0e025bf47e
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-10-25 10:22:43 +0630

    코드 및 기타 필요한 코드를 변경합니다. 코드 이상을 추가하고,일부 코드에 대해 약속을 하고,부 코드를 삭제합니다.

--- src/main/java/org/marine/simpulationwebprogram/controller/ClientDashboardController.java
+++ src/main/java/org/marine/simpulationwebprogram/controller/ClientDashboardController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WebController {
+public class ClientDashboardController {
 
     @GetMapping("/")
     public String home() {
